c0e34e7522f63a14d82dc4976e666dbedf3a966e
zhaojingjing1 <zhaojingjing1@300.cn>
2021-06-01 17:23:52 +0800

    feat(): mock订单数据

--- mock/table.js
+++ mock/table.js
@@ -1,14 +1,36 @@
 const Mock = require('mockjs')
 
 const data = Mock.mock({
-  'items|30': [{
-    id: '@id',
-    title: '@sentence(10, 20)',
-    'status|1': ['published', 'draft', 'deleted'],
-    author: 'name',
-    display_time: '@datetime',
-    pageviews: '@integer(300, 5000)'
-  }]
+  "list|9":[
+    {
+        id : "@id",
+        orderNumber:202105101719088093,
+        createTime:1620717797,
+        currencySymbol:"¥",
+        consigneeName:"收货人姓名",
+        sumPayable:"实付款",
+        freight: "@integer(1, 50)",
+        paymentTypeName:"微信支付",
+        orderStatus:"@integer(0, 4)",
+        "orderProductSkuList|2":[
+            {
+                productId:"@id",
+                skuId:12,
+                skuImg:"/public/assets/images/img.png",
+                skuName:"手机",
+                "skuSpec|3":[
+                    {
+                        specName:"内存",
+                        specValue:"8+256g",
+                    }
+                ],
+                skuPrice:"@integer(300, 5000)",
+                quantity:"@integer(1, 100)",
+                goodsUrl:"/goods/detail.html"
+            }
+        ]
+    }
+]
 })
 
 module.exports = [
@@ -16,12 +38,16 @@ module.exports = [
     url: '/vue-admin-template/table/list',
     type: 'get',
     response: config => {
-      const items = data.items
+      const list = data.list
       return {
         code: 20000,
         data: {
-          total: items.length,
-          items: items
+          pagination :{
+            pageSize : 5,
+            currentPage : 1,
+            totalCount : 4,
+          },
+          list: list
         }
       }
     }
