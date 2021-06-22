5beb0a0d39f9929b4b9925d70f72bf2bc270537f
jianting <yumismilecode@163.com>
2021-06-22 16:12:32 +0800

    接口地址

---
+++ src/api/settlement.js
@@ -0,0 +1,56 @@
+import request from '@/utils/request'
+
+export function payModeInitInfo(data) {
+  return request({
+    url: '/fwebapi/order/payMode/initInfo',
+    method: 'post',
+    data
+  })
+}
+
+export function addUpdateInvoice(data) {
+  return request({
+    url: '/fwebapi/order/settle/addUpdateInvoice',
+    method: 'post',
+    data
+  })
+}
+//运费计算
+export function calculateFare(data) {
+  return request({
+    url: '/fwebapi/order/settle/calculateFare',
+    method: 'post',
+    data
+  })
+}
+//提交订单
+export function addOrder(data) {
+  return request({
+    url: '/fwebapi/order/settle/addOrder',
+    method: 'post',
+    data
+  })
+}
+
+//查询发票
+export function getSettle(params) {
+  return request({
+    url: '/fwebapi/order/settle/list',
+    method: 'get',
+    params
+  })
+}
+
+//通过发票id查询发票
+export function getSettleById(params) {
+  return request({
+    url: '/fwebapi/order/settle/getById',
+    method: 'get',
+    params
+  })
+}
+
+
+
+
+
