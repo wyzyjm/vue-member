1e848dcaad03684971a0d98c6c29a7924cb1c5e2
zhaojingjing1 <zhaojingjing1@300.cn>
2021-07-27 11:26:02 +0800

    fix: 地址非空判断

--- src/components/CustomImg/index.vue
+++ src/components/CustomImg/index.vue
@@ -1,36 +1,41 @@
 <template>
-	<span>
-		<el-image class="customImg" :src="'https://pre-omo-oss-image.site.cn/' + src" fit="cover" lazy>
-			<div slot="error" class="image-slot" style="height: 100%">
-				<img class="errorImg" :src="errorImg" />
-			</div>
-		</el-image>
-	</span>
+  <span>
+    <el-image
+      class="customImg"
+      :src="src ? 'https://pre-omo-oss-image.site.cn/' + src : ''"
+      fit="cover"
+      lazy
+    >
+      <div slot="error" class="image-slot" style="height: 100%">
+        <img class="errorImg" :src="errorImg" />
+      </div>
+    </el-image>
+  </span>
 </template>
 
 <script>
-import errorImg from "@/assets/images/no-img.svg";
+import errorImg from '@/assets/images/no-img.svg'
 export default {
-	data() {
-		return {
-			errorImg: errorImg,
-		};
-	},
-	name: "CustomImg",
-	props: ["src", "alt", "title"],
-	components: {},
-};
+  data() {
+    return {
+      errorImg: errorImg
+    }
+  },
+  name: 'CustomImg',
+  props: ['src', 'alt', 'title'],
+  components: {}
+}
 </script>
 
 <style scoped>
 .customImg {
-	width: 100%;
-	height: 100%;
-	border-radius: 2px;
+  width: 100%;
+  height: 100%;
+  border-radius: 2px;
 }
 .errorImg {
-	width: 100%;
-	height: 100%;
-	object-fit: contain;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 </style>
