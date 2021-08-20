01aaebf4c9bbe9431c42a3f7fc50a95dff62e84b
zhaojingjing1 <zhaojingjing1@300.cn>
2021-08-20 11:32:25 +0800

    feat(): 根据需求去除“查看物流”

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -98,7 +98,7 @@
           <p v-if="data.logisticsInfo.distribution" class="consignee"><span>配送方式：</span><span>{{ data.logisticsInfo.distribution }}</span></p>
           <p v-if="data.logisticsInfo.courierCompanyCode" class="consignee" :datatype="data.logisticsInfo.courierCompanyCode"><span>快递公司：</span><span>{{ data.logisticsInfo.courierCompany }}</span></p>
           <p v-if="data.logisticsInfo.trackingNumber" class="consignee"><span>运单号：</span><span>{{ data.logisticsInfo.trackingNumber }}</span></p>
-          <p v-if="data.logisticsInfo.queryUrl" class="consignee"><span>物流查询：</span><a target="_blank" :href="data.logisticsInfo.queryUrl" class="el-button--text">{{ data.logisticsInfo.queryUrl }}</a></p>
+          <p v-if="data.logisticsInfo.queryUrl" v-show="false" class="consignee"><span>物流查询：</span><a target="_blank" :href="data.logisticsInfo.queryUrl" class="el-button--text">{{ data.logisticsInfo.queryUrl }}</a></p>
         </div>
       </li>
     </ul>
